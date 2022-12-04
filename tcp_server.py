954cd1f525f5e7ed87402a8736cccd1b570401ca
root <root@DESKTOP-57VQ9UN.localdomain>
2022-12-04 16:10:41 +0200

    added

--- tcp_server.py
+++ tcp_server.py
@@ -4,16 +4,24 @@ server.bind(('', 12345))
 server.listen(5)
 while True:
     client_socket, client_address = server.accept()
-    data = client_socket.recv(100)
+    data = client_socket.recv(1024)
     lines = data.decode().split('\r\n')
     file_name = "files" + lines[0].split(" ")[1]
-    if os.path.exists(file_name):
+    if (file_name == "files/"):
+        file_name = "files/index.html"
+    if file_name == "files/redirect":
+        print(data.decode())
+        connection = "close"
+        client_socket.send(
+            ("HTTP/1.1 301 Moved Permanently\r\nConnection: " + connection + "\r\nLocation: /result.html" + "\r\n\r\n").encode())
+        client_socket.close()
+    elif os.path.exists(file_name):
         print(data.decode())
         for line in lines:
             if line.split(" ")[0] == 'Connection:':
                 connection = line.split(" ")[1]
         size = os.path.getsize(file_name)
-        client_socket.send(("HTTP/1.1 200 OK\r\nConnection: " + connection +"\r\nContent-Length: {size}\r\n\r\n").encode())
+        client_socket.send(("HTTP/1.1 200 OK\r\nConnection: " + connection +"\r\nContent-Length: "+str(size)+"\r\n\r\n").encode())
         file = open(file_name, "rb")
         content = file.read()
         client_socket.send(content)
